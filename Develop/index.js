0ddb8f8671f31215e2403cfd031939eff89bc0c9
kdot <alvarezderrick0@gmail.com>
2021-12-11 00:11:28 -0800

    added function that moves data from inquirer prompt onto a readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-// const questions = [
-//     "What is your GitHub username?",
-//     "What is your email address?",
-//     "What is your project's name?",
-//     "Please write a short description fo your project?",
-//     "What kind of license should your project have?",
-//     "What command should be run to install dependencies?",
-//     "What command should be run to run tests?",
-//     "What does the user need to know about using the repo?",
-//     "What does the user need to know about contributing to the repo?"
-// ];
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 inquirer
     .prompt([
@@ -29,30 +18,25 @@ inquirer
         {
             type: 'input',
             name: 'project',
-            message: "What is your project's name"        
+            message: "What is your project's name?"        
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Please write a short description fo your project?'
+            message: 'Please write a short description for your project?'
         },
         {
             type: 'checkbox',
             name: 'license',
             message: 'What kind of license should your project have?',
-            choices: [ 'Apache License 2.0', 
-                       'GNU General Public License', 
-                       'MIT License', 
-                       'BSD 2-Clause Simplified License',
-                       'BSD 3-Clause New License',
-                       'Boost Software License 1.0',
-                       'Creative Commons Zero v1.0 Universal',
-                       'Eclipse Public License 2.0',
-                       'GNU Affero General Public License 3.0',
-                       'GNU General Public License v2.0',
-                       'GNU Lesser General Public License v2.1',
-                       'Mozilla Public License 2.0',
-                       'The Unlicense'
+            choices: [ 
+                'Apache',
+                'Academic',
+                'GNU',
+                'ISC',
+                'MIT',
+                'Mozilla',
+                'Open'
                     ],
         },
         {
@@ -73,18 +57,13 @@ inquirer
         {
             type: 'input',
             name:'contribution',
-            message: 'What does the user need to know about contributing to the repo?'
+            message: 'Who contributed to the application?'
         }   
     ])
     .then((data) => {
         console.log(data);
-    })
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+        const generateReadMe = generateMarkdown(data);
+        fs.writeFile('README.md', generateReadMe, (err) =>
+            err ? console.log(err) : console.log('Successfully created ReadMe.md')
+        );
+    });
