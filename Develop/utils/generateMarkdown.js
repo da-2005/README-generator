3cae03eab93f9fe5dfe1d0a90658e2935b677ce5
kdot <alvarezderrick0@gmail.com>
2021-12-10 23:38:06 -0800

    function that creates .md file

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,50 @@
+
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  <br />
+  * ${data.idepend}
+  
+  ## Usage
+  ${data.use}
+  
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+  <br />
+  This application is covered by the ${data.license} license.
+  
+  ## Contributing
+  <br />
+  * ${data.contribution}
+  
+  ## Tests
+  To run tests on this app, run the following command:
+  <br />
+  * ${data.test}
+  
+  ## Questions
+  For questions regarding the app, please contact me here:
+  <br />
+  * Email: ${data.email}
+  * GitHub: ${data.username}
+  `;
+}
+
+module.exports = generateMarkdown;
